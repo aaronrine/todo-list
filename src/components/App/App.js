1f9ec2549138f6540d6a1eee8af5efe3b6cfe924
Aaron Rine <aaronrine@gmail.com>
2020-12-24 19:14:01 -0800

    refactor(Todo): move todo app into todo component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,52 +1,10 @@
-import { data } from "../../assets/data";
-import Form from "../Form/Form";
-import List from "../List/List";
-import useStateWithLocalStorage from "../../hooks/useStateWithLocalStorage";
-
-import "./App.scss";
-
+import Todo from "../Todo/Todo";
 export function App() {
-  const [itemList, setItemList] = useStateWithLocalStorage("itemList", data);
-
-  function addItem(data) {
-    setItemList((oldItemList) => [
-      ...oldItemList,
-      { ...data, id: oldItemList.length + 1 },
-    ]);
-  }
-
-  function deleteItem(data) {
-    setItemList((oldItemList) => {
-      return oldItemList.filter((item) => {
-        return item.id !== data.id;
-      });
-    });
-  }
-
-  function handleSubmit(data) {
-    addItem(data);
-  }
-
-  function updateItem(updatedItem) {
-    setItemList((prevItemList) =>
-      prevItemList.map((item) => {
-        if (item.id !== updatedItem.id) {
-          return item;
-        }
-        return updatedItem;
-      })
-    );
-  }
-
   return (
     <div className="App">
-      <Form handleSubmit={handleSubmit} />
-      <List
-        itemList={itemList}
-        deleteItem={deleteItem}
-        updateItem={updateItem}
-      />
+      <Todo />
     </div>
   );
 }
+
 export default App;
---
+++ src/components/Todo/Todo.js
@@ -0,0 +1,52 @@
+import { data } from "../../assets/data";
+import Form from "../Form/Form";
+import List from "../List/List";
+import useStateWithLocalStorage from "../../hooks/useStateWithLocalStorage";
+
+import "./Todo.scss";
+
+export function Todo() {
+  const [itemList, setItemList] = useStateWithLocalStorage("itemList", data);
+
+  function addItem(data) {
+    setItemList((oldItemList) => [
+      ...oldItemList,
+      { ...data, id: oldItemList.length + 1 },
+    ]);
+  }
+
+  function deleteItem(data) {
+    setItemList((oldItemList) => {
+      return oldItemList.filter((item) => {
+        return item.id !== data.id;
+      });
+    });
+  }
+
+  function handleSubmit(data) {
+    addItem(data);
+  }
+
+  function updateItem(updatedItem) {
+    setItemList((prevItemList) =>
+      prevItemList.map((item) => {
+        if (item.id !== updatedItem.id) {
+          return item;
+        }
+        return updatedItem;
+      })
+    );
+  }
+
+  return (
+    <div className="Todo">
+      <Form handleSubmit={handleSubmit} />
+      <List
+        itemList={itemList}
+        deleteItem={deleteItem}
+        updateItem={updateItem}
+      />
+    </div>
+  );
+}
+export default Todo;
